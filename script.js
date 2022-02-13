8134ca740b7a6fec2c6ad1bf6e9fc37f5bd0828c
taurusilver7 <prasad20697@gmail.com>
2022-02-13 17:15:38 +0530

    slider function

--- script.js
+++ script.js
@@ -84,10 +84,6 @@ var swiper = new Swiper(".arrivals-slider", {
     delay: 9500,
     disableOnInteraction: false,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
   breakpoints: {
     0: {
       slidesPerView: 1,
